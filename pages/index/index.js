4c86dc1442adae90919298de47b93ec442a6303c
gordoning <linguoyang2008@126.com>
2018-07-04 23:09:16 +0800

    增加了支付功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,9 @@
 //index.js
+
+const AV = require('../../utils/av-live-query-weapp-min');
+const { User, Query, Cloud } = require('../../utils/av-live-query-weapp-min');
+// const Order = require('../../model/order');
+
 //获取应用实例
 const app = getApp()
 
@@ -15,40 +20,48 @@ Page({
       url: '../logs/logs'
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+
+  //登录
+  login: function() {
+    return AV.Promise.resolve(AV.User.current()).then(user =>
+      user ? (user.isAuthenticated().then(authed => authed ? user : null)) : null
+    ).then(user => user ? user : AV.User.loginWithWeapp()).catch(error => console.error(error.message));
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+
+  onReady: function() {
+    this.login();
+    console.log(AV.User.current())
+  },
+
+  //缴纳会费
+  donate() {
+    wx.showToast({
+      title: '正在创建订单',
+      icon: 'loading',
+      duration: 10000,
+      mask: true,
+    })
+    Cloud.run('order').then((data) => {
+      wx.hideToast();
+      data.success = () => {
+        wx.showToast({
+          title: '支付成功',
+          icon: 'success',
+          duration: 1500,
+        });
+        // setTimeout(this.refreshOrders.bind(this), 1500);
+      }
+      data.fail = ({
+        errMsg
+      }) => this.setData({
+        error: errMsg
+      });
+      wx.requestPayment(data);
+    }).catch(error => {
+      this.setData({
+        error: error.message
+      });
+      wx.hideToast();
     })
   }
 })
